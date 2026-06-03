f8428126c4ff4790c6bb4b577f2b00663d059595
St487 <siewting8008@gmail.com>
2026-06-03 23:20:10 +0800

    fix website prob

--- src/app/component/map/map.ts
+++ src/app/component/map/map.ts
@@ -47,10 +47,9 @@ constructor(
     L = leaflet.default;
 
     delete L.Icon.Default.prototype._getIconUrl;
-
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
       iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
       shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
     });
 
